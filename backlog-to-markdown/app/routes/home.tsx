178b80f5cd6504a64818eeb6fdd170e19703910c
wineAk <akasaka@interpark.co.jp>
2025-09-17 11:03:32 +0900

    [fix] 長い文字列でボタンが隠れる問題を修正

--- backlog-to-markdown/app/routes/home.tsx
+++ backlog-to-markdown/app/routes/home.tsx
@@ -30,29 +30,33 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   const isIdle = fetcher.state === "idle";
   const markdownOutput = fetcher.data?.markdown;
   return (
-    <main className="p-8 grid grid-cols-1 sm:grid-cols-2 gap-4">
-      <fetcher.Form method="post" className="space-y-4">
-        <TypographyH3>Backlog</TypographyH3>
-        <Button type="submit">{isIdle ? "変換する" : "変換中…"}</Button>
-        <Textarea name="backlog-input" />
+    <main className="h-dvh p-8 grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <fetcher.Form method="post" className="flex flex-col gap-4">
+        <section className="flex justify-between sm:justify-start items-center gap-2">
+          <TypographyH3>Backlog</TypographyH3>
+          <Button type="submit">{isIdle ? "変換する" : "変換中…"}</Button>
+        </section>
+        <Textarea name="backlog-input" className="h-full resize-none field-sizing-fixed" />
       </fetcher.Form>
-      <div className="space-y-4">
-        <TypographyH3>Markdown</TypographyH3>
-        <Button
-          type="button"
-          disabled={!markdownOutput}
-          onClick={() => {
-            try {
-              navigator.clipboard.writeText(markdownOutput);
-              toast.success("コピーしました");
-            } catch (error) {
-              toast.error("コピーに失敗しました");
-            }
-          }}
-        >
-          コピー
-        </Button>
-        <Textarea name="markdown-output" value={markdownOutput} disabled />
+      <div className="flex flex-col gap-4">
+        <section className="flex justify-between sm:justify-start items-center gap-2">
+          <TypographyH3>Markdown</TypographyH3>
+          <Button
+            type="button"
+            disabled={!markdownOutput}
+            onClick={() => {
+              try {
+                navigator.clipboard.writeText(markdownOutput);
+                toast.success("コピーしました");
+              } catch (error) {
+                toast.error("コピーに失敗しました");
+              }
+            }}
+          >
+            コピー
+          </Button>
+        </section>
+        <Textarea name="markdown-output" value={markdownOutput} disabled className="h-full resize-none field-sizing-fixed" />
       </div>
     </main>
   );
